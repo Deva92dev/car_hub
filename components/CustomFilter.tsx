6acaceb6c9522609f7431c9e629cb6f68c77b1da
Devashish Rai <devashishrai.dev@gmail.com>
2023-06-30 17:12:22 +0530

    innitial commit

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { Fragment, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
+
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+  // const router = useRouter();
+
+  // This way you can share specific page centric url to anyone, SSR
+  // const handleUpdateParams = (e: { title: string; value: string }) => {
+  //   const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+  //   router.push(newPathName); // This will trigger SSR
+  // };
+
+  return (
+    <div className='w-fit'>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          setFilter(e.value);
+        }}
+      >
+        <div className='relative w-fit z-10'>
+          <Listbox.Button className='custom-filter__btn'>
+            <span className='block truncate'>{selected.title}</span>
+            <Image
+              src='/chevron-up-down.svg'
+              alt='chevron up down'
+              width={20}
+              height={20}
+              className='ml-4 object-contain'
+            />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <Listbox.Options className='custom-filter__options'>
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  // Just below line, headless ui allows us to have active state as a parameter in arrow function like
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+};
+
+export default CustomFilter;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { SearchManufacturer } from './';
+import Image from 'next/image';
+
+// onSubmit is a browser event, hence make this page on client side
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src='/magnifying-glass.svg'
+      alt='magnifying glass'
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
+
+const SearchBar = ({ setManufacturer, setModel }) => {
+  const [searchManufacturer, setSearchManufacturer] = useState('');
+  const [searchModel, setSearchModel] = useState('');
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (searchManufacturer === '' && searchModel === '') {
+      return alert('Please fill in the search bar');
+    }
+
+    setModel(searchModel), setManufacturer(searchManufacturer);
+  };
+
+  // In the below  function, we are taking the value from search bar on the page then appending it to search console(top of every page), then updating it through model, manufacturer and then making a path name then pushing that path name so that it can be easily searched. It is used for SSR
+  // const updateSearchParams = (model: string, manufacturer: string) => {
+  //   // below values are in search bar on top,  in search bar
+  //   const searchParams = new URLSearchParams(window.location.search);
+
+  //   if (model) {
+  //     searchParams.set('model', model);
+  //   } else {
+  //     searchParams.delete('model');
+  //   }
+
+  //   if (manufacturer) {
+  //     searchParams.set('manufacturer', manufacturer);
+  //   } else {
+  //     searchParams.delete('manufacturer');
+  //   }
+
+  //   const newPathname = `${
+  //     window.location.pathname
+  //   }?${searchParams.toString()}`;
+
+  //   router.push(newPathname);
+  // };
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          selected={searchManufacturer}
+          setSelected={setSearchManufacturer}
+        />
+
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          alt='car model'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+        />
+        <input
+          type='text'
+          name='model'
+          value={searchModel}
+          onChange={(e) => setSearchModel(e.target.value)}
+          placeholder='Tiguan'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
+    </form>
+  );
+};
+
+export default SearchBar;
